1351dc9abdee9df521af3c7d91ac8f9bf5ff14da
sachin senal <127573781+sachinsenal0x64@users.noreply.github.com>
2024-02-07 18:02:29 +0100

    Update windows_start.bat

--- windows_start.bat
+++ windows_start.bat
@@ -1,5 +1,5 @@
 @echo off
-
+setlocal
 
 :: BatchGotAdmin
 :-------------------------------------
@@ -10,24 +10,49 @@ REM --> If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
     echo Requesting administrative privileges...
     goto UACPrompt
-) else ( goto gotAdmin )
+) else (
+    goto gotAdmin
+)
 
 :UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
-    "%temp%\getadmin.vbs"
-    exit /B
+"%temp%\getadmin.vbs"
+exit /B
 
 :gotAdmin
-    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-    pushd "%CD%"
-    CD /D "%~dp0"
+if exist "%temp%\getadmin.vbs" (
+    del "%temp%\getadmin.vbs"
+)
+pushd "%CD%"
+CD /D "%~dp0"
+goto :eof  :: or use 'exit /b'
+
 :--------------------------------------
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 
+:: Define repository URL and clone directory
+set "repoURL=https://github.com/sachinsenal0x64/FIXARR"
+set "cloneDir=FIXARR"
+
+:: Check if fixarr.py exists
+if not exist "%cloneDir%\fixarr.py" (
+    echo fixarr.py does not exist. Cloning Git repository...
+    git clone %repoURL% %cloneDir%
+)
+
+:: Change to the clone directory
+cd /d %cloneDir%
+
+:: Print message and install packages
+echo Installing Packages......
+pip install -r requirements.txt
+
+:: Run fixarr.py without displaying the console window
 set python_path=python
 set code_file=fixarr.py
 start cmd.exe /k %python_path% %code_file%
 
+endlocal
 @REM @CMD.EXE
