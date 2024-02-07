5a6cbca15e11ba4245e475c655986acbdb0ca8e5
sachin senal <127573781+sachinsenal0x64@users.noreply.github.com>
2024-02-07 17:46:08 +0100

    Update linux_&_osx_start.sh

--- linux_&_osx_start.sh
+++ linux_&_osx_start.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-git clone https://github.com/sachinsenal0x64/FIXARR
-cd FIXARR
+REPO_URL="https://github.com/sachinsenal0x64/FIXARR"
+CLONE_DIR="FIXARR"
+
+# Check if fixarr.py exists
+if [ -e "${CLONE_DIR}/fixarr.py" ]; then
+    echo "fixarr.py already exists. Skipping Git clone."
+else
+    echo "fixarr.py does not exist. Cloning Git repository..."
+    git clone "${REPO_URL}" "${CLONE_DIR}"
+fi
+
+cd "${CLONE_DIR}"
 echo "Installing Packages......"
 pip install -r requirements.txt
 python fixarr.py
